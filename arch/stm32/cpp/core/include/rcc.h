25c4bd0d1948d3876f89b61909ee005d256b8e73
Maya Posch <maya@nyanko.ws>
2020-08-05 20:47:37 +0200

    Implement RCC AHB, APB1 & APB2 ENR registers.

--- arch/stm32/cpp/core/include/rcc.h
+++ arch/stm32/cpp/core/include/rcc.h
@@ -20,8 +20,43 @@ enum RccPeripheral {
 	RCC_SRAM,
 	RCC_DMA2,
 	RCC_DMA,
-	RCC_SYSCFG,		// APB peripherals
-	
+	RCC_SYSCFGCOMP,		// APB2 peripherals
+	RCC_USART6,
+	RCC_USART7,
+	RCC_USART8,
+	RCC_ADC,
+	RCC_TIM1,
+	RCC_SPI1,
+	RCC_USART1,
+	RCC_TIM15,
+	RCC_TIM16,
+	RCC_TIM17,
+	RCC_DBGMCU,
+	RCC_TIM2,		// APB1 peripherals
+	RCC_TIM3,
+	RCC_TIM14,
+	RCC_WWDG,
+	RCC_SPI2,
+	RCC_USART2,
+	RCC_USART3,
+	RCC_USART4,
+	RCC_USART5,
+	RCC_I2C1,
+	RCC_I2C2,
+	RCC_USB,
+	RCC_CAN,
+	RCC_CRS,
+	RCC_PWR,
+	RCC_DAC,
+	RCC_CEC
+};
+
+
+struct RccPeripheralHandle {
+	uint8_t count = 0;
+	bool exists = false;
+	uint32_t enr;
+	uint32_t enable;
 };
 
 
@@ -44,6 +79,7 @@ struct RccPortHandle {
 
 class RCC {
 	static std::vector<RccPortHandle>& portHandles();
+	static std::vector<RccPeripheralHandle>& peripheralHandles();
 	
 	static bool initPorts();
 	
--- arch/stm32/cpp/core/src/rcc.cpp
+++ arch/stm32/cpp/core/src/rcc.cpp
@@ -6,8 +6,17 @@
 
 #include "rcc.h"
 
+#include <limits>
 
-// Static initialisations.
+
+// The maximum handles per port or peripheral is determined by the counter type in the item struct.
+// If changed, the counter type should be updated both in the type definition and here.
+static const handle_max = std::numeric_limits<uint8_t>::max();
+
+
+// Private methods:
+
+// --- PORT HANDLES ---
 std::vector<RccPortHandle>& RCC::portHandles {
 	static std::vector<RccPortHandle>* portHandlesStatic = new std::vector<RccPortHandle>();
 	
@@ -64,12 +73,387 @@ std::vector<RccPortHandle>& RCC::portHandles {
 }
 
 
+// --- PERIPHERAL HANDLES ---
+std::vector<RccPeripheralHandle>& RCC::peripheralHandles() {
+	static std::vector<RccPeripheralHandle>* peripheralHandlesStatic = new std::vector<RccPeripheralHandle>();
+	
+	// AHB
+	
+	RccPeripheralHandle handle;
+#ifdef RCC_AHBENR_TSCEN
+	handle.exists = true;
+	handle.enr = &(RCC->AHBENR);
+	handle.enable = RCC_AHBENR_TSCEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_AHBENR_CRCEN
+	handle.exists = true;
+	handle.enr = &(RCC->AHBENR);
+	handle.enable = RCC_AHBENR_CRCEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_AHBENR_FLITFEN
+	handle.exists = true;
+	handle.enr = &(RCC->AHBENR);
+	handle.enable = RCC_AHBENR_FLITFEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_AHBENR_SRAMEN
+	handle.exists = true;
+	handle.enr = &(RCC->AHBENR);
+	handle.enable = RCC_AHBENR_SRAMEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_AHBENR_DMA2EN
+	handle.exists = true;
+	handle.enr = &(RCC->AHBENR);
+	handle.enable = RCC_AHBENR_DMA2EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_AHBENR_DMAEN
+	handle.exists = true;
+	handle.enr = &(RCC->AHBENR);
+	handle.enable = RCC_AHBENR_DMAEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+	
+	// APB2
+
+#ifdef RCC_APB2ENR_SYSCFGCOMPEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_SYSCFGCOMPEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_USART6EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_USART6EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_USART7EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_USART7EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_USART8EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_USART8EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_ADCEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_ADCEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_TIM1EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_TIM1EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_SPI1EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_SPI1EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_USART1EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_USART1EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_TIM15EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_TIM15EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_TIM16EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_TIM16EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_TIM17EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_SYSCFGCOMPEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB2ENR_DBGMCUEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB2ENR);
+	handle.enable = RCC_APB2ENR_DBGMCUEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+	
+	// APB1
+
+#ifdef RCC_APB1ENR_TIM2EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_TIM2EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_TIM3EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_TIM3EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_TIM6EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_TIM6EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_TIM7EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_TIM7EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_TIM14EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_TIM14EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_WWDGEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_WWDGEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_SPI2EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_SPI2EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_USART2EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_USART2EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_USART3EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_USART3EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_USART4EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_USART4EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_USART5EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_USART5EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_I2C1EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_I2C1EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_I2C2EN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_I2C2EN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_USBEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_USBEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_CANEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_CANEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_CRSEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_CRSEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_PWREN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_PWREN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_DACEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_DACEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+
+#ifdef RCC_APB1ENR_CECEN
+	handle.exists = true;
+	handle.enr = &(RCC->APB1ENR);
+	handle.enable = RCC_APB1ENR_CECEN;
+#else
+	handle.exists = false;
+#endif
+	peripheralHandlesStatic->push_back(handle);
+	
+	
+	return *peripheralHandlesStatic;
+}
+
+
+// Public methods:
+
 // --- ENABLE ---
 // Enable the target peripheral. 
 // Returns true if the peripheral was successfully turned on, or if the peripheral was already on.
 // Returns false if the peripheral could not be turned on. 
 bool RCC::enable(RccPeripheral peripheral) {
-	//
+	static std::vector<RccPeripheralHandle> &perHandlesStatic = RCC::peripheralHandles();
+	uint8_t perNum = (uint8_t) peripheral;
+	RccPeripheralHandle &ph = perHandlesStatic[perNum];
+	
+	if (ph.exists == false) {
+		// TODO: set reason.
+		return false;
+	}
+	
+	// Check the current peripheral status.
+	if (ph.count > 0) {
+		if (ph.count >= handle_max) {
+			// Reached maximum handles. Return false to prevent counter overflow.
+			// TODO: set reason.
+			return false;
+		}
+		
+		// Increase handler count by one.
+		ph.count++;
+	}
+	else {
+		// Activate the peripheral.
+		ph.count = 1;
+		ph.enr |= ph.enable;
+	}
 	
 	return true;
 }
@@ -81,7 +465,23 @@ bool RCC::enable(RccPeripheral peripheral) {
 // if the peripheral was successfully disabled.
 // Returns false if the request was rejected or disabling the peripheral failed.
 bool RCC::disable(RccPeripheral peripheral) {
-	//
+	static std::vector<RccPeripheralHandle> &perHandlesStatic = RCC::peripheralHandles();
+	uint8_t perNum = (uint8_t) peripheral;
+	RccPeripheralHandle &ph = perHandlesStatic[perNum];
+	
+	if (ph.exists == false) {
+		// TODO: set reason.
+		return false;
+	}
+	
+	if (ph.count > 1) {
+		// Decrease handler count by one.
+		ph.count--;
+	}
+	else {
+		// Deactivate the peripheral.
+		ph.count = 0;
+		ph.enr &= ~(ph.enable);
 	
 	return true;
 }
@@ -93,21 +493,28 @@ bool RCC::disable(RccPeripheral peripheral) {
 bool RCC::enablePort(RccPort port) {
 	static std::vector<RccPortHandle> &portHandlesStatic = RCC::portHandles();
 	uint8_t portNum = (uint8_t) port;
+	RccPortHandle &ph = portHandlesStatic[portNum];
 	
-	if (portHandlesStatic[portNum].exists == false) {
+	if (ph.exists == false) {
 		// TODO: set reason.
 		return false; 
 	}
 	
 	// Check the current port status.
-	if (portHandlesStatic[portNum].count > 0) {
+	if (ph.count > 0) {
+		if (ph.count >= handle_max) {
+			// Reached maximum handles. Return false to prevent counter overflow.
+			// TODO: set reason.
+			return false;
+		}
+		
 		// Increase handler count by one.
-		portHandlesStatic[(uint8_t) port].count++;
+		ph.count++;
 	}
 	else {
 		// Activate the port.
-		portHandlesStatic[portNum].count = 1;
-		RCC->AHBENR |= portHandlesStatic[portNum].enable;
+		ph.count = 1;
+		RCC->AHBENR |= ph.enable;
 	}
 	
 	return true;
@@ -120,21 +527,22 @@ bool RCC::enablePort(RccPort port) {
 bool RCC::disablePort(RccPort port) {
 	static std::vector<RccPortHandle> &portHandlesStatic = RCC::portHandles();
 	uint8_t portNum = (uint8_t) port;
+	RccPortHandle &ph = portHandlesStatic[portNum];
 	
-	if (portHandlesStatic[portNum].exists == false) {
+	if (ph.exists == false) {
 		// TODO: set reason.
 		return false; 
 	}
 	
 	// Check the current port status.
-	if (portHandlesStatic[portNum].count > 1) {
+	if (ph.count > 1) {
 		// Decrease handler count by one.
-		portHandlesStatic[portNum].count++;
+		ph.count--;
 	}
 	else {
 		// Deactivate the port.
-		portHandlesStatic[portNum].count = 0;
-		RCC->AHBENR &= ~(portHandlesStatic[portNum].enable);
+		ph.count = 0;
+		RCC->AHBENR &= ~(ph.enable);
 	}
 	
 	return true;

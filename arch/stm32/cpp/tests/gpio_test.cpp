559000e21f03e7943ab2121968faa0eb5b01dcd0
Maya Posch <maya@nyanko.ws>
2020-08-19 17:09:12 +0200

    Extend GPIO tests.

--- arch/stm32/cpp/tests/gpio_test.cpp
+++ arch/stm32/cpp/tests/gpio_test.cpp
@@ -22,6 +22,11 @@ int main() {
 	GPIO gpio;
 	if (gpio.set_output(GPIO_PORT_A, 3, GPIO_PULL_UP)) { std::cout << "Set output." << std::endl; }
 	if (gpio.write(GPIO_PORT_A, 3, GPIO_LEVEL_HIGH)) { std::cout << "Wrote HIGH." << std::endl; }
+	if (gpio.set_output(GPIO_PORT_B, 1, GPIO_PULL_UP)) { std::cout << "Set output." << std::endl; }
+	if (gpio.write(GPIO_PORT_B, 1, GPIO_LEVEL_HIGH)) { std::cout << "Wrote HIGH." << std::endl; }
+	
+	if (gpio.set_input(GPIO_PORT_A, 1, GPIO_PULL_UP)) { std::cout << "Set input." << std::endl; }
+	if (gpio.set_input(GPIO_PORT_B, 3, GPIO_PULL_UP)) { std::cout << "Set input." << std::endl; }
 	
 	// Print out the RCC AHB register.
 	std::cout << std::endl;
@@ -37,6 +42,17 @@ int main() {
 	std::cout << "OSPEEDR:\t" << std::bitset<32>(GPIOA->OSPEEDR) << std::endl;
 	std::cout << "IDR:    \t" << std::bitset<32>(GPIOA->IDR) << std::endl;
 	std::cout << "ODR:    \t" << std::bitset<32>(GPIOA->ODR) << std::endl;
+	std::cout << std::endl;
+	
+	// Print out the GPIOB MODER, PUPDR, OTYPER, OSPEEDR, IDR and ODR registers.
+	std::cout << "GPIOB" << std::endl;
+	std::cout << "MODER:  \t" << std::bitset<32>(GPIOB->MODER) << std::endl;
+	std::cout << "PUPDR:  \t" << std::bitset<32>(GPIOB->PUPDR) << std::endl;
+	std::cout << "OTYPER: \t" << std::bitset<32>(GPIOB->OTYPER) << std::endl;
+	std::cout << "OSPEEDR:\t" << std::bitset<32>(GPIOB->OSPEEDR) << std::endl;
+	std::cout << "IDR:    \t" << std::bitset<32>(GPIOB->IDR) << std::endl;
+	std::cout << "ODR:    \t" << std::bitset<32>(GPIOB->ODR) << std::endl;
+	std::cout << std::endl;
 	
 	return 0;
 }

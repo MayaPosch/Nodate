fda1f488f453e51e34b3fc4b521c4ca219cbd841
Maya Posch <maya@nyanko.ws>
2020-08-23 17:04:23 +0200

    Add support for STM32F746 & refactor example code.

--- examples/stm32/pushy/src/pushy.cpp
+++ examples/stm32/pushy/src/pushy.cpp
@@ -7,31 +7,37 @@ int main () {
 	// Initialise.
 	GPIO gpio;
 	
-	// FIXME: Dynamically updated LED_BUILTIN value.
-	//const uint8_t LED_BUILTIN = 3; // Nucleo-f042k6: Port B, pin 3.
-	const uint8_t LED_BUILTIN = 13; // STM32F4-Discovery: Port D, pin 13 (orange)
-	const uint8_t BUTTON_PIN = 0;
+	//const uint8_t led_pin = 3; // Nucleo-f042k6: Port B, pin 3.
+	//const GPIO_ports led_port = GPIO_PORT_B;
+	//const uint8_t led_pin = 13; // STM32F4-Discovery: Port D, pin 13 (orange)
+	//const GPIO_ports led_port = GPIO_PORT_D;
+	const uint8_t led_pin = 7; // Nucleo-F746ZG: Port B, pin 7 (blue)
+	const GPIO_ports led_port = GPIO_PORT_B;
 	
-	// Set the pin mode on the built-in LED.
-	gpio.set_output(GPIO_PORT_D, LED_BUILTIN, GPIO_PULL_UP);
-	gpio.write(GPIO_PORT_D, LED_BUILTIN, GPIO_LEVEL_LOW);
+	//const uint8_t button_pin = 1; // Nucleo-f042k6 (PB1)
+	//const GPIO_ports button_port = GPIO_PORT_B;
+	//const uint8_t button_pin = 0; // STM32F4-Discovery (PA0)
+	//const GPIO_ports button_port = GPIO_PORT_A;
+	const uint8_t button_pin = 0; // Nucleo-F746ZG (PC13)
+	const GPIO_ports button_port = GPIO_PORT_A;
 	
-	// Set input mode on PB1 pin. (Nucleo-f042k6)
-	//gpio.set_input(GPIO_PORT_B, 1, GPIO_PULL_UP);
+	// Set the pin mode on the LED pin.
+	gpio.set_output(led_port, led_pin, GPIO_PULL_UP);
+	gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
 	
-	// Set input mode on PA0 pin. (STM32F4-Discovery)
-	gpio.set_input(GPIO_PORT_A, 0, GPIO_FLOATING);
+	// Set input mode on button pin.
+	gpio.set_input(button_port, button_pin, GPIO_FLOATING);
 	
 	// If the button pulls down to ground (high to low), 'button_down' is low when pushed.
 	// If the button is pulled up to Vdd (low to high), 'button_down' is high when pushed.
 	uint8_t button_down;
 	while (1) {
-		button_down = gpio.read(GPIO_PORT_A, BUTTON_PIN);
+		button_down = gpio.read(button_port, button_pin);
 		if (button_down == 1) {
-			gpio.write(GPIO_PORT_D, LED_BUILTIN, GPIO_LEVEL_HIGH);
+			gpio.write(led_port, led_pin, GPIO_LEVEL_HIGH);
 		}
 		else {
-			gpio.write(GPIO_PORT_D, LED_BUILTIN, GPIO_LEVEL_LOW);
+			gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
 		}
 	}
 	

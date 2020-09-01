75e1edddeff7823d09173de42f229084b0433505
Maya Posch <maya@nyanko.ws>
2020-09-01 21:43:07 +0200

    Add Ada Pushy example.

--- examples-ada/pushy/src/pushy.adb
+++ examples-ada/pushy/src/pushy.adb
@@ -1,28 +1,44 @@
 -- Basic Pushy example for Nodate's STM32 framework.
 
+
 with GPIO;
+with stm32_h; use stm32_h;
 
 
-procedure main is
-	led_builtin	: gpio_pin := 3;
-	button_pin	: gpio_pin := 1;
-	button_down	: Integer;
+procedure pushy is
+	led_pin		: Integer 		:= 3; --  Nucleo-f042k6: Port B, pin 4 (D12).
+	led_port	: GPIO_ports	:= GPIO_PORT_B;
+	--led_pin		: Integer		:= 13; --  STM32F4-Discovery: Port D, pin 13 (orange)
+	--led_port	: GPIO_ports	:= GPIO_PORT_D;
+	--led_pin		: Integer		:= 7; -- Nucleo-F746ZG: Port B, pin 7 (blue)
+	--led_port	: GPIO_ports	:= GPIO_PORT_B;
 	
+	button_pin	: Integer 		:= 1; -- Nucleo-f042k6 (PB1)
+	button_port	: GPIO_ports	:= GPIO_PORT_B;
+	--button_pin	: Integer		:= 0; -- STM32F4-Discovery (PA0)
+	--button_port	: GPIO_ports	:= GPIO_PORT_A;
+	--button_pin	: Integer		:= 13; -- Nucleo-F746ZG (PC13)
+	--button_port	: GPIO_ports	:= GPIO_PORT_C;
+	
+	button_down	: Integer;
+	ret			: Boolean;
 begin
 	-- Set pin mode on the built-in LED.
-	gpio.set_output();
-	gpio.write();
+	ret := gpio.set_output(led_port, led_pin, GPIO_PULL_UP);
+	ret := gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
 	
 	-- Set input mode on PB1 pin.
-	gpio.set_input();
+	ret := gpio.set_input(button_port, button_pin, GPIO_FLOATING);
 	
-	loop;
-		button_down := gpio.read();
+	-- If the button pulls down to ground (high to low), 'button_down' is low when pushed.
+	-- If the button is pulled up to Vdd (low to high), 'button_down' is high when pushed.
+	loop
+		button_down := gpio.read(button_port, button_pin);
 		if button_down = 0 then
-			gpio.write();
+			ret := gpio.write(led_port, led_pin, GPIO_LEVEL_HIGH);
 		else
-			gpio.write();
+			ret := gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
 		end if;
 	end loop;
 
-end main;
+end pushy;

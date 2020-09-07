147cc9439f11c339c95dae817a052629580db514
Maya Posch <maya@nyanko.ws>
2020-09-07 16:55:19 +0200

    Update test during validation.

--- examples/stm32/uart/src/uart.cpp
+++ examples/stm32/uart/src/uart.cpp
@@ -18,7 +18,11 @@ int main () {
 	// Start UART.
 	// Nucleo-F042K6 (STM32F042): USART2 (TX: PA2 (AF1), RX: PA15 (AF1)).
 	// USART2 is normally connected to USB (ST-Link) on the Nucleo board.
-	USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 15, 1, 9600, uartCallback);
+	//USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 15, 1, 9600, uartCallback);
+	// USART 2, (TX) PA2:1 [A7], (RX) PA3:1 [A2].
+	//USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 3, 1, 9600, uartCallback);
+	// USART 1, (TX) PA9:1 [D1], (RX) PA10:1 [D0].
+	USART::startUart(USART_1, GPIO_PORT_A, 9, 1, GPIO_PORT_A, 10, 1, 9600, uartCallback);
 	
 	const uint8_t led_pin = 3; // Nucleo-f042k6: Port B, pin 3.
 	const GPIO_ports led_port = GPIO_PORT_B;
@@ -33,8 +37,8 @@ int main () {
 	gpio.set_output(led_port, led_pin, GPIO_PULL_UP);
 	gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
 	
-	char c = 'h';
-	USART::sendUart(USART_2, c);
+	//char c = 'h';
+	//USART::sendUart(USART_2, c);
 	
 	while (1) {
 		// The interrupt handler will handle things from here.

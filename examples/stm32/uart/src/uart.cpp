43715de0afd96b01e149e61bf50058db69013eb0
Maya Posch <maya@nyanko.ws>
2020-09-08 12:24:12 +0200

    Update UART example during RX debugging.

--- examples/stm32/uart/src/uart.cpp
+++ examples/stm32/uart/src/uart.cpp
@@ -3,12 +3,14 @@
 #include <usart.h>
 
 
-volatile bool led_on;
+volatile bool led_on = false;
 
 
 void uartCallback(char ch) {
 	// Copy character into send buffer.
-	USART::sendUart(USART_2, ch);
+	//ch = 'e';
+	//USART::sendUart(USART_2, ch);
+	USART::sendUart(USART_1, ch);
 	
 	led_on = !led_on;
 }
@@ -18,11 +20,13 @@ int main () {
 	// Start UART.
 	// Nucleo-F042K6 (STM32F042): USART2 (TX: PA2 (AF1), RX: PA15 (AF1)).
 	// USART2 is normally connected to USB (ST-Link) on the Nucleo board.
-	//USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 15, 1, 9600, uartCallback);
+	USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 15, 1, 9600, uartCallback);
 	// USART 2, (TX) PA2:1 [A7], (RX) PA3:1 [A2].
 	//USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 3, 1, 9600, uartCallback);
 	// USART 1, (TX) PA9:1 [D1], (RX) PA10:1 [D0].
-	USART::startUart(USART_1, GPIO_PORT_A, 9, 1, GPIO_PORT_A, 10, 1, 9600, uartCallback);
+	//USART::startUart(USART_1, GPIO_PORT_A, 9, 1, GPIO_PORT_A, 10, 1, 9600, uartCallback);
+	// USART 1, (TX) PB6:0 [D5], (RX) PB7:0 [D4].
+	//USART::startUart(USART_1, GPIO_PORT_B, 6, 0, GPIO_PORT_B, 7, 0, 9600, uartCallback);
 	
 	const uint8_t led_pin = 3; // Nucleo-f042k6: Port B, pin 3.
 	const GPIO_ports led_port = GPIO_PORT_B;
@@ -37,11 +41,9 @@ int main () {
 	gpio.set_output(led_port, led_pin, GPIO_PULL_UP);
 	gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
 	
-	//char c = 'h';
-	//USART::sendUart(USART_2, c);
-	
 	while (1) {
 		// The interrupt handler will handle things from here.
+		
 		
 		// The LED is used to indicate reception of data.
 		if (led_on) {

aa91e7018400b7fa25b335fdc56754d047c985ca
Maya Posch <maya@nyanko.ws>
2020-09-24 17:46:19 +0200

    Tweak RTC init; still not working.

--- arch/stm32/cpp/core/src/rtc.cpp
+++ arch/stm32/cpp/core/src/rtc.cpp
@@ -22,9 +22,17 @@ bool rtc_enabled = false;
 // --- ENABLE RTC ---
 bool Rtc::enableRTC() {
 	//
-#if defined RTC_TR_SU
+#if defined __stm32f4
 	if (rtc_enabled) { return true; }
 	if (!Rcc::enable(RCC_PWR)) { return false; }
+	
+	// Use LSI for the RTC. Ensure it's enabled.
+	if (!RCC->CSR & RCC_CSR_LSION) {
+		RCC->CSR |= RCC_CSR_LSION;
+		while (!RCC->CSR & RCC_CSR_LSIRDY) {
+			// TODO: handle time-out.
+		}
+	}
 	
 	// Enable PWR backup access.
 	PWR->CR |= PWR_CR_DBP;
@@ -34,13 +42,32 @@ bool Rtc::enableRTC() {
 		// TODO: handle time-out.
 	}
 	
-	// TODO: configure RTC.
+	// Unlock RTC write protection.
+	RTC->WPR = 0xCA;
+	RTC->WPR = 0x53;
+	
+	// Reset backup domain.
+	RCC->BDCR |= RCC_BDCR_BDRST;
+	
+	// Configure RTC clock source.
+	RCC->BDCR &= ~(0xF << RCC_BDCR_RTCSEL_Pos);
+	RCC->BDCR |= (0x2 << RCC_BDCR_RTCSEL_Pos); // LSI source.
 	
 	// Enable RTC clock.
 	RCC->BDCR |= RCC_BDCR_RTCEN;
-	while (!(RCC->BDCR & RCC_BDCR_RTCEN)) {
+	
+	// End backup domain reset.
+	RCC->BDCR &= ~(RCC_BDCR_BDRST);
+	
+	// Clear RTC init state.
+	RTC->ISR &= ~(RTC_ISR_INIT);
+	
+	// Wait for RTC APB register synchronisation.
+	/* int i = 10000;
+	while (!(RTC->ISR & RTC_ISR_RSF) || i < 1) {
 		// TODO: handle time-out.
-	}
+		i--;
+	} */
 	
 	rtc_enabled = true;
 	
@@ -54,7 +81,11 @@ bool Rtc::enableRTC() {
 
 // --- DISABLE RTC ---
 bool Rtc::disableRTC() {
-	//
+	if (!rtc_enabled) { return true; }
+	
+	RCC->BDCR &= ~(RCC_BDCR_RTCEN);
+	
+	rtc_enabled = false;
 	
 	return true;
 }

c359db17122afbabbcb69ec443d6e73e5f4ee319
Maya Posch <maya@nyanko.ws>
2020-10-01 18:42:42 +0200

    Update Blinky example for STM32F103 board.

--- examples/stm32/blinky/src/blinky.cpp
+++ examples/stm32/blinky/src/blinky.cpp
@@ -9,12 +9,14 @@ int main () {
 	GPIO gpio;
 	Timer timer;
 	
-	const uint8_t led_pin = 3; // Nucleo-f042k6: Port B, pin 3.
-	const GPIO_ports led_port = GPIO_PORT_B;
+	//const uint8_t led_pin = 3; // Nucleo-f042k6: Port B, pin 3.
+	//const GPIO_ports led_port = GPIO_PORT_B;
 	//const uint8_t led_pin = 13; // STM32F4-Discovery: Port D, pin 13 (orange)
 	//const GPIO_ports led_port = GPIO_PORT_D;
 	//const uint8_t led_pin = 7; // Nucleo-F746ZG: Port B, pin 7 (blue)
 	//const GPIO_ports led_port = GPIO_PORT_B;
+	const uint8_t led_pin = 13;	// Blue Pill: Port C, pin 13.
+	const GPIO_ports led_port = GPIO_PORT_C;
 	
 	// Set the pin mode on the LED pin.
 	gpio.set_output(led_port, led_pin, GPIO_PULL_UP);

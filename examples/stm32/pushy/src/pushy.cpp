23adeb47b54cc20c818d117d8d7c07a129750ca4
Maya Posch <maya@nyanko.ws>
2020-10-02 15:41:33 +0200

    Configure Pushy test for STM32F103 Blue Pill board.

--- examples/stm32/pushy/src/pushy.cpp
+++ examples/stm32/pushy/src/pushy.cpp
@@ -11,15 +11,19 @@ int main () {
 	//const GPIO_ports led_port = GPIO_PORT_B;
 	//const uint8_t led_pin = 13; // STM32F4-Discovery: Port D, pin 13 (orange)
 	//const GPIO_ports led_port = GPIO_PORT_D;
-	const uint8_t led_pin = 7; // Nucleo-F746ZG: Port B, pin 7 (blue)
-	const GPIO_ports led_port = GPIO_PORT_B;
+	//const uint8_t led_pin = 7; // Nucleo-F746ZG: Port B, pin 7 (blue)
+	//const GPIO_ports led_port = GPIO_PORT_B;
+	const uint8_t led_pin = 13;	// Blue Pill: Port C, pin 13.
+	const GPIO_ports led_port = GPIO_PORT_C;
 	
 	//const uint8_t button_pin = 1; // Nucleo-f042k6 (PB1)
 	//const GPIO_ports button_port = GPIO_PORT_B;
 	//const uint8_t button_pin = 0; // STM32F4-Discovery (PA0)
 	//const GPIO_ports button_port = GPIO_PORT_A;
-	const uint8_t button_pin = 13; // Nucleo-F746ZG (PC13)
-	const GPIO_ports button_port = GPIO_PORT_C;
+	//const uint8_t button_pin = 13; // Nucleo-F746ZG (PC13)
+	//const GPIO_ports button_port = GPIO_PORT_C;
+	const uint8_t button_pin = 10; // Blue Pill
+	const GPIO_ports button_port = GPIO_PORT_B;
 	
 	// Set the pin mode on the LED pin.
 	gpio.set_output(led_port, led_pin, GPIO_PULL_UP);

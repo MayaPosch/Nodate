059eb86063d8abe91f517ca478a4cd5bd93bbc15
Maya Posch <maya@nyanko.ws>
2020-10-04 14:16:26 +0200

    Update test for STM32F1.

--- arch/stm32/cpp/tests/uart_test.cpp
+++ arch/stm32/cpp/tests/uart_test.cpp
@@ -27,9 +27,11 @@ int main() {
 	if (USART::startUart(USART_1, GPIO_PORT_A, 9, 1, 
 								GPIO_PORT_A, 10, 1, 
 								9600, uartCallback)) { std::cout << "Start USART1." << std::endl; }
+	else { std::cout << "Starting USART1 failed." << std::endl; }
 	if (USART::startUart(USART_2, GPIO_PORT_B, 0, 1, 
 								GPIO_PORT_B, 8, 1, 
 								9600, uartCallback)) { std::cout << "Start USART2." << std::endl; }
+	else { std::cout << "Starting USART2 failed." << std::endl; }
 	
 	// Print out the RCC AHB & APB1/2 registers.
 	std::cout << std::endl;
@@ -39,7 +41,15 @@ int main() {
 	std::cout << "APB1ENR:\t" << std::bitset<32>(RCC->APB1ENR) << std::endl;
 	std::cout << "APB2ENR:\t" << std::bitset<32>(RCC->APB2ENR) << std::endl;
 	std::cout << std::endl;
-	
+
+#ifdef STM32F1
+	// Print out the GPIOA CRL & CRH registers plus IDR and ODR.
+	std::cout << "GPIOA" << std::endl;
+	std::cout << "CRL:  \t" << std::bitset<32>(GPIOA->CRL) << std::endl;
+	std::cout << "CRH:  \t" << std::bitset<32>(GPIOA->CRH) << std::endl;
+	std::cout << "IDR:  \t" << std::bitset<32>(GPIOA->IDR) << std::endl;
+	std::cout << "ODR:  \t" << std::bitset<32>(GPIOA->ODR) << std::endl;
+#else
 	// Print out the GPIOA MODER, PUPDR, OTYPER, OSPEEDR, IDR and ODR registers.
 	std::cout << "GPIOA" << std::endl;
 	std::cout << "MODER:  \t" << std::bitset<32>(GPIOA->MODER) << std::endl;
@@ -50,8 +60,17 @@ int main() {
 	std::cout << "ODR:    \t" << std::bitset<32>(GPIOA->ODR) << std::endl;
 	std::cout << "AFRL:   \t" << std::bitset<32>(GPIOA->AFR[0]) << std::endl;
 	std::cout << "AFRH:   \t" << std::bitset<32>(GPIOA->AFR[1]) << std::endl;
+#endif
 	std::cout << std::endl;
 	
+#ifdef STM32F1
+	// Print out the GPIOB CRL & CRH registers plus IDR and ODR.
+	std::cout << "GPIOB" << std::endl;
+	std::cout << "CRL:  \t" << std::bitset<32>(GPIOB->CRL) << std::endl;
+	std::cout << "CRH:  \t" << std::bitset<32>(GPIOB->CRH) << std::endl;
+	std::cout << "IDR:  \t" << std::bitset<32>(GPIOB->IDR) << std::endl;
+	std::cout << "ODR:  \t" << std::bitset<32>(GPIOB->ODR) << std::endl;
+#else
 	// Print out the GPIOB MODER, PUPDR, OTYPER, OSPEEDR, IDR and ODR registers.
 	std::cout << "GPIOB" << std::endl;
 	std::cout << "MODER:  \t" << std::bitset<32>(GPIOB->MODER) << std::endl;
@@ -62,7 +81,14 @@ int main() {
 	std::cout << "ODR:    \t" << std::bitset<32>(GPIOB->ODR) << std::endl;
 	std::cout << "AFRL:   \t" << std::bitset<32>(GPIOB->AFR[0]) << std::endl;
 	std::cout << "AFRH:   \t" << std::bitset<32>(GPIOB->AFR[1]) << std::endl;
+#endif
 	std::cout << std::endl;
+	
+#ifdef STM32F1
+	std::cout << "AFIO" << std::endl;
+	std::cout << "MAPR:	\t" << std::bitset<32>(AFIO->MAPR) << std::endl;
+	std::cout << std::endl;
+#endif
 	
 	// Print out the USART1 CR1, BRR registers.
 	std::cout << "USART1" << std::endl;

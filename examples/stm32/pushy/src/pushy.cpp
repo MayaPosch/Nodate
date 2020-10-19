7a0f58c96c12bfb3acfde201b38ed31cb654ae07
Maya Posch <maya@nyanko.ws>
2020-10-19 19:43:04 +0200

    Update example to use static GPIO class.

--- examples/stm32/pushy/src/pushy.cpp
+++ examples/stm32/pushy/src/pushy.cpp
@@ -4,9 +4,6 @@
 
 
 int main () {
-	// Initialise.
-	GPIO gpio;
-	
 	//const uint8_t led_pin = 3; // Nucleo-f042k6: Port B, pin 3.
 	//const GPIO_ports led_port = GPIO_PORT_B;
 	//const uint8_t led_pin = 13; // STM32F4-Discovery: Port D, pin 13 (orange)
@@ -26,22 +23,22 @@ int main () {
 	const GPIO_ports button_port = GPIO_PORT_B;
 	
 	// Set the pin mode on the LED pin.
-	gpio.set_output(led_port, led_pin, GPIO_PULL_UP);
-	gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
+	GPIO::set_output(led_port, led_pin, GPIO_PULL_UP);
+	GPIO::write(led_port, led_pin, GPIO_LEVEL_LOW);
 	
 	// Set input mode on button pin.
-	gpio.set_input(button_port, button_pin, GPIO_FLOATING);
+	GPIO::set_input(button_port, button_pin, GPIO_FLOATING);
 	
 	// If the button pulls down to ground (high to low), 'button_down' is low when pushed.
 	// If the button is pulled up to Vdd (low to high), 'button_down' is high when pushed.
 	uint8_t button_down;
 	while (1) {
-		button_down = gpio.read(button_port, button_pin);
+		button_down = GPIO::read(button_port, button_pin);
 		if (button_down == 1) {
-			gpio.write(led_port, led_pin, GPIO_LEVEL_HIGH);
+			GPIO::write(led_port, led_pin, GPIO_LEVEL_HIGH);
 		}
 		else {
-			gpio.write(led_port, led_pin, GPIO_LEVEL_LOW);
+			GPIO::write(led_port, led_pin, GPIO_LEVEL_LOW);
 		}
 	}
 	

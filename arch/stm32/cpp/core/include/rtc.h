45498eb6835cf625734c8fb1e317613d581484a3
Maya Posch <maya@nyanko.ws>
2020-11-02 18:04:23 +0100

    Implement basic RTC API for F4.

--- arch/stm32/cpp/core/include/rtc.h
+++ arch/stm32/cpp/core/include/rtc.h
@@ -16,11 +16,29 @@
 #ifndef RTC_H
 #define RTC_H
 
+#include <common.h>
+
+
+struct RtcTime {
+	uint8_t hour_tens;
+	uint8_t hour_units;
+	uint8_t minute_tens;
+	uint8_t minute_units;
+	uint8_t second_tens;
+	uint8_t second_units;
+};
+
+
 class Rtc {
 	//
 	
 public:
 	static bool enableRTC();
+	static bool setTime(uint32_t time);
+	static bool getTime(RtcTime &time);
+	static bool getTime(char* t, uint8_t &len);
+	static bool getDate();
+	static bool getDate(char* t, uint8_t &len);
 	static bool disableRTC();
 };
 
--- arch/stm32/cpp/core/src/rtc.cpp
+++ arch/stm32/cpp/core/src/rtc.cpp
@@ -21,7 +21,7 @@ bool rtc_enabled = false;
 
 // --- ENABLE RTC ---
 bool Rtc::enableRTC() {
-	//
+	// TODO: instead of the LSI, the LSE should be available as optional RTC clock source.
 #if defined __stm32f4
 	if (rtc_enabled) { return true; }
 	
@@ -38,9 +38,9 @@ bool Rtc::enableRTC() {
 	PWR->CR |= PWR_CR_DBP;
 	
 	// Wait for DBP to be enabled.
-	while (!(PWR->CR & PWR_CR_DBP)) {
+	/* while (!(PWR->CR & PWR_CR_DBP)) {
 		// TODO: handle time-out.
-	}
+	} */
 	
 	// Reset backup domain.
 	//RCC->BDCR |= RCC_BDCR_BDRST;
@@ -52,27 +52,34 @@ bool Rtc::enableRTC() {
 	// Disable PWR.
 	if (!Rcc::disable(RCC_PWR)) { return false; }
 	
-	// RTC init phase.
-	
-	// Unlock RTC write protection.
-	RTC->WPR = 0xCA;
-	RTC->WPR = 0x53;
-	
-	// Enable RTC init phase.
-	RTC->ISR |= RTC_ISR_INIT;
-	while ((RTC->ISR & RTC_ISR_INITF) != RTC_ISR_INITF) {
+	// Disable alarm A to modify it.
+	// Wait for the action to complete.
+	RTC->CR &=~ RTC_CR_ALRAE;
+	while((RTC->ISR & RTC_ISR_ALRAWF) != RTC_ISR_ALRAWF) {
 		// TODO: handle time-out.
 	}
 	
-	// End backup domain reset.
-	//RCC->BDCR &= ~(RCC_BDCR_BDRST);
+	// Have alarm A generate interrupt each 1 Hz.
+	RTC->ALRMAR = RTC_ALRMAR_MSK4 | RTC_ALRMAR_MSK3 | RTC_ALRMAR_MSK2 | RTC_ALRMAR_MSK1;
 	
-	// Clear RTC init state.
-	RTC->ISR &= ~(RTC_ISR_INIT);
+	// Enable alarm A and its interrupt.
+	RTC->CR = RTC_CR_ALRAIE | RTC_CR_ALRAE;
 	
 	// Disable write access.
 	RTC->WPR = 0xFE;
 	RTC->WPR = 0x64; 
+	
+	// Configure interrupts.
+	EXTI->IMR |= EXTI_IMR_MR17;		// Unmask line 17.
+	EXTI->RTSR |= EXTI_RTSR_TR17;	// Trigger on rising edge.
+	NVIC_SetPriority(RTC_Alarm_IRQn, 0);	// RTC IRQ priority.
+	NVIC_EnableIRQ(RTC_Alarm_IRQn);	// Enable IRQ in NVIC. (Note: RTC_IRQn on F0).
+	
+	// End configuration phase.
+	
+	// RTC init phase.
+	Rtc::setTime(0); // Set time to 0.
+	
 	
 	// Wait for RTC APB register synchronisation.
 	/* int i = 10000;
@@ -88,6 +95,72 @@ bool Rtc::enableRTC() {
 	// No usable RTC peripheral exists. Return -1.
 	return false;
 #endif 
+}
+
+
+// --- SET TIME ---
+bool Rtc::setTime(uint32_t time) {
+	// Unlock RTC write protection.
+	RTC->WPR = 0xCA;
+	RTC->WPR = 0x53;
+	
+	// Enable RTC init phase.
+	RTC->ISR |= RTC_ISR_INIT;
+	while ((RTC->ISR & RTC_ISR_INITF) != RTC_ISR_INITF) {
+		// TODO: handle time-out.
+	}
+	
+	RTC->PRER = 0x007F0137;	// Set prescaler: 40 kHz / 128 = 312 kHz, 312 / 312 = 1 Hz.
+	RTC->TR = RTC_TR_PM | time;
+	
+	// Clear RTC init state.
+	RTC->ISR &= ~(RTC_ISR_INIT);
+	
+	// Disable write access.
+	RTC->WPR = 0xFE;
+	RTC->WPR = 0x64; 
+	
+	return true;
+}
+
+
+// --- GET TIME ---
+bool Rtc::getTime(RtcTime &time) {
+	uint32_t currentTime = RTC->TR;
+	time.hour_tens = (uint8_t) (((currentTime & RTC_TR_HT) >> 20) + 48);
+	time.hour_units = (uint8_t) (((currentTime & RTC_TR_HT) >> 16) + 48);
+	time.minute_tens = (uint8_t) (((currentTime & RTC_TR_HT) >> 12) + 48);
+	time.minute_units = (uint8_t) (((currentTime & RTC_TR_HT) >> 8) + 48);
+	time.second_tens = (uint8_t) (((currentTime & RTC_TR_HT) >> 4) + 48);
+	time.second_units = (uint8_t) (((currentTime & RTC_TR_HT)) + 48);
+	
+	return true;
+}
+
+
+// --- GET TIME ---
+bool Rtc::getTime(char* t, uint8_t &len) {
+	//
+	
+	return true;
+}
+
+
+// --- GET DATE ---
+bool Rtc::getDate() {
+	//
+	
+	return true;
+}
+
+
+// --- GET DATE ---
+bool Rtc::getDate(char* date, uint8_t &len) {
+	len = 20;
+	//char* stringtosend[20] = "\n00 : 00 : 00 ";
+	//date = char[20];
+	
+	return true;
 }
 
 

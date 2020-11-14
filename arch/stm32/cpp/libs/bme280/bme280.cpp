3e1a70b2108ca5e41b67214acb4862fedf9eab8a
Maya Posch <maya@nyanko.ws>
2020-11-14 18:55:41 +0100

    Implement basic readId API.

--- arch/stm32/cpp/libs/bme280/bme280.cpp
+++ arch/stm32/cpp/libs/bme280/bme280.cpp
@@ -4,9 +4,12 @@
 #include "bme280.h"
 
 
+volatile bool I2C_wait = false;
+volatile uint8_t I2C_byte = 0;
 
 void bmeCallback(uint8_t byte) {
-	//
+	I2C_byte = byte;
+	I2C_wait = false;
 }
 
 
@@ -16,11 +19,25 @@ BME280::BME280(I2C_devices device, uint8_t address) {
 	I2C::startMaster(device, I2C_MODE_FM, bmeCallback);
 	this->device = device;
 	this->address = address;
+	
+	I2C::setSlaveTarget(device, address);
 }
 
 
 // --- READ ID ---
 // Reads the sensor's fixed ID.
 bool BME280::readID(uint8_t &id) {
-	// 
+	// Send register to read to the device.
+	uint8_t data = 0xd0;
+	I2C::sendToSlave(device, &data, 1);
+	I2C_wait = true;
+	
+	// Read the response once it comes in.
+	while (I2C_wait) {
+		// TODO: handle timeout.
+	}
+	
+	id = I2C_byte;
+	
+	return true;
 }

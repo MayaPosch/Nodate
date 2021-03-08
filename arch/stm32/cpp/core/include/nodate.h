fe970a9e16d4c456bb0f8141af452395d8c08aa7
Maya Posch <maya@nyanko.ws>
2021-03-08 19:38:17 +0100

    Add general Nodate header file.

---
+++ arch/stm32/cpp/core/include/nodate.h
@@ -0,0 +1,32 @@
+/*
+	nodate.h - Common header file for the Nodate STM32 framework.
+	
+	Features:
+			- Provides includes for all core functionality.
+			- Automates board-specific includes.
+			
+	Notes:
+			- 
+			
+	2021/03/01, Maya Posch
+*/
+
+
+#ifndef NODATE_H
+#define NODATE_H
+
+#include <board_definition.h>
+
+#include <clock.h>
+#include <common.h>
+#include <ethernet.h>
+#include <gpio.h>
+#include <i2c.h>
+#include <interrupts.h>
+#include <io.h>
+#include <rcc.h>
+#include <rtc.h>
+#include <timer.h>
+#include <usart.h>
+
+#endif

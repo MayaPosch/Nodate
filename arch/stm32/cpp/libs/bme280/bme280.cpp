52e474a94abae9eb74d10aacf3f51c23fe5ef89d
Maya Posch <maya@nyanko.ws>
2021-03-28 14:26:39 +0200

    Add full initialisation & temperature reading routines.

--- arch/stm32/cpp/libs/bme280/bme280.cpp
+++ arch/stm32/cpp/libs/bme280/bme280.cpp
@@ -21,6 +21,57 @@ BME280::BME280(I2C_devices device, uint8_t address) {
 	this->address = address;
 	
 	I2C::setSlaveTarget(device, address);
+	
+	// Read calibration data from device and store it.
+	uint8_t ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | BME280_OperationMode;
+	uint8_t ctrl_hum_reg  = osrs_h;
+
+	uint8_t config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en;
+
+	I2C::sendToSlave(device, &controlHumidity, ctrl_hum_reg);
+	I2C::sendToSlave(device, &reg_Control, ctrl_meas_reg);
+	I2C::sendToSlave(device, &reg_Config, config_reg);
+
+	I2C::sendToSlave(device, &reg_CalibrationTStart, 1);
+	uint8_t buffer[64];
+	I2C::receiveFromSlave(device, reg_CalibrationTEnd - reg_CalibrationTStart + 1, buffer);
+	
+	// This data is in Big Endian format from the BME280.
+    BME280_REGISTER_DIG_T1 = (buffer[1] << 8) | buffer[0];
+    BME280_REGISTER_DIG_T2 = (buffer[3] << 8) | buffer[2];
+    BME280_REGISTER_DIG_T3 = (buffer[5] << 8) | buffer[4];
+
+	I2C::sendToSlave(device, &reg_CalibrationPStart, 1);
+	I2C::receiveFromSlave(device, reg_CalibrationPEnd - reg_CalibrationPStart + 1, buffer);
+
+    BME280_REGISTER_DIG_P1 = (buffer[1] << 8) | buffer[0];
+    BME280_REGISTER_DIG_P2 = (buffer[3] << 8) | buffer[2];
+    BME280_REGISTER_DIG_P3 = (buffer[5] << 8) | buffer[4];
+    BME280_REGISTER_DIG_P4 = (buffer[7] << 8) | buffer[6];
+    BME280_REGISTER_DIG_P5 = (buffer[9] << 8) | buffer[8];
+    BME280_REGISTER_DIG_P6 = (buffer[11] << 8) | buffer[10];
+    BME280_REGISTER_DIG_P7 = (buffer[13] << 8) | buffer[12];
+    BME280_REGISTER_DIG_P8 = (buffer[15] << 8) | buffer[14];
+	BME280_REGISTER_DIG_P9 = (buffer[17] << 8) | buffer[16];
+
+	I2C::sendToSlave(device, &BME280_REG_DIG_H1, 1);
+	I2C::receiveFromSlave(device, 1, buffer);
+	BME280_REGISTER_DIG_H1 = buffer[0];
+	
+	I2C::sendToSlave(device, &BME280_REG_DIG_H2, 1);
+	I2C::receiveFromSlave(device, 2, buffer);
+    BME280_REGISTER_DIG_H2 = (buffer[1] << 8) | buffer[0];
+	
+	I2C::sendToSlave(device, &BME280_REG_DIG_H3, 1);
+	I2C::receiveFromSlave(device, 1, buffer);
+    BME280_REGISTER_DIG_H3 = buffer[0];
+	
+	I2C::sendToSlave(device, &BME280_REG_DIG_H4, 1);
+	I2C::receiveFromSlave(device, 4, buffer);
+    BME280_REGISTER_DIG_H4 = (buffer[0] << 4) | (buffer[1]&0x0F);
+	
+    BME280_REGISTER_DIG_H5 = (buffer[2]<<4) | ((buffer[1] & 0xF0)>>4);
+	BME280_REGISTER_DIG_H6 = buffer[3];
 }
 
 
@@ -40,7 +91,7 @@ bool BME280::readID(uint8_t &id) {
 	I2C_wait = true;
     
     // Initiate the read sequence
-    if (!(I2C::receiveFromSlave(device,1))) {
+    if (!(I2C::receiveFromSlave(device, 1))) {
         return false;
     }
 	
@@ -57,16 +108,49 @@ bool BME280::readID(uint8_t &id) {
 }
 
 
-float BME280::GetTemperature() {
-	//
+float BME280::temperature() {
+	return compensateTemperature(rawTemperature());
 }
 
 
-float BME280::GetPressure() {
+float BME280::pressure() {
 	//
+	
+	return 0.0;
 }
 
 
-float BME280::GetHumidity() {
+float BME280::humidity() {
 	//
+	
+	return 0.0;
+}
+
+
+int32_t BME280::rawTemperature() {
+	// Send register to read to the device.
+	uint8_t data = 0xFA;
+	I2C::sendToSlave(device, &data, 1);
+    
+    // Initiate the read sequence
+	uint8_t buffer[3];
+    if (!(I2C::receiveFromSlave(device, 3, buffer))) {
+        return false;
+    }
+	
+ 	int32_t value = ((buffer[0] << 12) | (buffer[1] << 4) | (buffer[2] >> 4));
+
+    return value;
+}
+
+
+// FIXME: BME280 datasheet suggests returning int32_t type, not float. Replace?
+float BME280::compensateTemperature(int32_t rawTemp) {
+	int32_t var1, var2;
+	var1 = ((((rawTemp >> 3) - ((int32_t) BME280_REGISTER_DIG_T1 << 1))) * ((int32_t) BME280_REGISTER_DIG_T2)) >> 11;
+	var2 = (((((rawTemp >> 4) - ((int32_t) BME280_REGISTER_DIG_T1)) * ((rawTemp >> 4) - ((int32_t) BME280_REGISTER_DIG_T1))) >> 12) * ((int32_t) BME280_REGISTER_DIG_T3)) >> 14;
+	int32_t t_fine = var1 + var2;
+	float temperature = (t_fine * 5 + 128) >> 8;  /* temperature in 0.01 deg C*/
+
+	return temperature / 100;
 }
--- arch/stm32/cpp/libs/bme280/bme280.h
+++ arch/stm32/cpp/libs/bme280/bme280.h
@@ -15,19 +15,74 @@
 #include <i2c.h>
 
 
+#define BME280_MODE_NORMAL			0x03 // Reads sensor at set interval.
+#define BME280_MODE_FORCED			0x01 // Reads sensor after write to register.
+
+
 class BME280 {
 	I2C_devices device;
 	uint8_t address;
 	bool ready;
+	
+	uint16_t BME280_REGISTER_DIG_T1;
+	int16_t BME280_REGISTER_DIG_T2;
+	int16_t BME280_REGISTER_DIG_T3;
+
+	uint16_t BME280_REGISTER_DIG_P1;
+	int16_t BME280_REGISTER_DIG_P2;
+	int16_t BME280_REGISTER_DIG_P3;
+	int16_t BME280_REGISTER_DIG_P4;
+	int16_t BME280_REGISTER_DIG_P5;
+	int16_t BME280_REGISTER_DIG_P6;
+	int16_t BME280_REGISTER_DIG_P7;
+	int16_t BME280_REGISTER_DIG_P8;
+	int16_t BME280_REGISTER_DIG_P9;
+
+	uint8_t BME280_REGISTER_DIG_H1;
+	int16_t BME280_REGISTER_DIG_H2;
+	int8_t BME280_REGISTER_DIG_H3;
+	int16_t BME280_REGISTER_DIG_H4;
+	int16_t BME280_REGISTER_DIG_H5;
+	int8_t BME280_REGISTER_DIG_H6;
+	
+	uint8_t BME280_REG_DIG_H1	= 0xA1;
+	uint8_t BME280_REG_DIG_H2   = 0xE1;
+	uint8_t BME280_REG_DIG_H3   = 0xE3;
+	uint8_t BME280_REG_DIG_H4   = 0xE4;
+	uint8_t BME280_REG_DIG_H5   = 0xE5;
+	uint8_t BME280_REG_DIG_H6   = 0xE7;
+	
+	uint8_t osrs_t = 1;             //Temperature oversampling x 1
+	uint8_t osrs_p = 1;             //Pressure oversampling x 1
+	uint8_t osrs_h = 1;             //Humidity oversampling x 1
+
+	uint8_t t_sb = 4;               //Tstandby, 5=1000ms, 4=500ms
+	uint8_t filter = 0;             //Filter off
+	uint8_t spi3w_en = 0;           //3-wire SPI Disable
+	uint8_t BME280_OperationMode = BME280_MODE_NORMAL;
+	
+	uint8_t measureHumidity = 0xFD;
+	uint8_t controlHumidity = 0xF2;
+	uint8_t reg_Control = 0xF4;
+	uint8_t reg_Config = 0xF5;
+	uint8_t reg_CalibrationTStart = 0x88;
+	uint8_t reg_CalibrationTEnd = 0x8D;
+
+	uint8_t reg_CalibrationPStart = 0x8E;
+	uint8_t reg_CalibrationPEnd = 0x9F;
 	
 public:
 	BME280(I2C_devices device, uint8_t address);
 	bool isReady();
 	bool readID(uint8_t &id);
 	
-	float GetTemperature();
-	float GetPressure();
-	float GetHumidity();
+	float temperature();
+	float pressure();
+	float humidity();
+	
+	int32_t rawTemperature();
+	
+	float compensateTemperature(int32_t rawTemp);
 };
 
 #endif

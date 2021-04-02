0c509e9af97fe53f9acd7d712ba25f925e115abf
Maya Posch <maya@nyanko.ws>
2021-04-02 21:08:12 +0200

    Add new Nodate configuration files.

---
+++ examples/stm32/dhrystone/src/nodate_config.h
@@ -0,0 +1,9 @@
+// Set configuration options for the Nodate framework.
+
+// -- Optional modules ---
+//#define NODATE_ETHERNET_ENABLED
+#define NODATE_GPIO_ENABLED
+//#define NODATE_I2C_ENABLED
+//#define NODATE_INTERRUPTS_ENABLED
+#define NODATE_TIMER_ENABLED
+#define NODATE_USART_ENABLED

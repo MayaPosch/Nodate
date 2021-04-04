3b52b0e818d096c983895810ffb6b60debdbc049
Maya Posch <maya@nyanko.ws>
2021-04-04 17:29:54 +0200

    Switch to VCP for nucleo-f042k6 on USART 2.

--- examples/stm32/i2c_bme280/src/i2c_bme280.cpp
+++ examples/stm32/i2c_bme280/src/i2c_bme280.cpp
@@ -10,8 +10,8 @@
 
 void uartCallback(char ch) {
 	// Copy character into send buffer.
-	//USART::sendUart(USART_2, ch);
-	USART::sendUart(USART_1, ch);
+	USART::sendUart(USART_2, ch);
+	//USART::sendUart(USART_1, ch);
 }
 
 
@@ -19,9 +19,9 @@ int main () {
 	// Initialise UART.
 	// Nucleo-F042K6 (STM32F042): USART2 (TX: PA2 (AF1), RX: PA15 (AF1)).
 	// USART2 is normally connected to USB (ST-Link) on the Nucleo board.
-	//USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 15, 1, 9600, uartCallback);
+	USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 15, 1, 9600, uartCallback);
 	// USART 1, (TX) PA9:1 [D1], (RX) PA10:1 [D0].
-	USART::startUart(USART_1, GPIO_PORT_A, 9, 1, GPIO_PORT_A, 10, 1, 9600, uartCallback);
+	//USART::startUart(USART_1, GPIO_PORT_A, 9, 1, GPIO_PORT_A, 10, 1, 9600, uartCallback);
 	
 	// STM32F4-Discovery (STM32F407): USART2, (TX) PA2:7, (RX) PA3:7.
 	//USART::startUart(USART_2, GPIO_PORT_A, 2, 7, GPIO_PORT_A, 3, 7, 9600, uartCallback);
@@ -40,22 +40,22 @@ int main () {
 	GPIO::write(led_port, led_pin, GPIO_LEVEL_LOW);
 	
 	// Set up stdout.
-	IO::setStdOutTarget(USART_1);
+	IO::setStdOutTarget(USART_2);
 	
 	printf("Starting I2C BME280 example...\n");
 	char ch = 'c';
-	USART::sendUart(USART_1, ch);
+	USART::sendUart(USART_2, ch);
 	
 	// Start i2c
 	// Nucleo-F042K6: I2C_1, SCL -> PA11:5, SDA -> PA12:5.
 	if (!I2C::startI2C(I2C_1, GPIO_PORT_A, 11, 5, GPIO_PORT_A, 12, 5)) {
 		ch = 'f';
-		USART::sendUart(USART_1, ch);
+		USART::sendUart(USART_2, ch);
 		while (1) { }
 	}
 	
 	ch = 's';
-	USART::sendUart(USART_1, ch);
+	USART::sendUart(USART_2, ch);
 	
 	// Create BME280 sensor instance, on the first I2C bus, slave address 0x76 (default).
     // Note that, depending on the design of the BME280 carrier board (if any), the
@@ -63,23 +63,23 @@ int main () {
 	BME280 sensor(I2C_1, 0x76);
 	if (!sensor.isReady()) {
 		ch = 'f';
-		USART::sendUart(USART_1, ch);
+		USART::sendUart(USART_2, ch);
 		while (1) { }
 	}
 	
 	ch = 'r';
-	USART::sendUart(USART_1, ch);
+	USART::sendUart(USART_2, ch);
 	
 	// Read and print out sensor ID.
 	uint8_t id = 0;
 	ch = 'f';
 	if (!sensor.readID(id)) {
-		//printf("Error reading ID.\n");
+		printf("Error reading ID.\n");
 	}
 	else {
 		ch = 't';
 		GPIO::write(led_port, led_pin, GPIO_LEVEL_HIGH);
-		//printf("Sensor ID: %d\n", id);
+		printf("Sensor ID: %d\n", id);
 
 		if (id == 0x60) {
 			ch = 'm';
@@ -90,7 +90,7 @@ int main () {
 	float t = sensor.temperature();
 	printf("Temp: %f.\n", t);
 	
-	USART::sendUart(USART_1, ch);
+	USART::sendUart(USART_2, ch);
 	//printf("Done.\n");
 	
 	while(1) {

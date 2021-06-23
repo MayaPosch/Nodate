e61e0c39e19eda25f18b3f649c05caed3c09ec78
Maya Posch <maya@nyanko.ws>
2021-06-23 10:03:49 +0200

    Add stdout config, add printf output, use board config.

--- projects/stm32/scpi_server/src/scpi_server.cpp
+++ projects/stm32/scpi_server/src/scpi_server.cpp
@@ -13,13 +13,8 @@
 
 
 #include <nodate.h>
-//#include <freertos/FreeRTOS/Source/include/FreeRTOS.h>
-//#include <cmsis_os.h>
 #include <cmsis_rtos.h>
-//#include <LwIP/src/include/lwip/netif.h>
-//#include <LwIP/src/include/lwip/tcpip.h>
 #include <lwip.h>
-//#include <libscpi/libscpi.h>
 extern "C" {
 #include "scpi_server.h"
 }
@@ -27,9 +22,7 @@ extern "C" {
 
 void uartCallback(char ch) {
 	// Copy character into send buffer.
-	//USART::sendUart(USART_2, ch);
 	USART::sendUart(USART_3, ch);
-	//USART::sendUart(USART_1, ch);
 }
 
 //static void startThread(void const* argument);
@@ -40,34 +33,39 @@ void startTasks() {
 
 
 int main() {
-	// 1. Hardware configuration.
+	// 0. Hardware configuration.
 	// We wish to set up the UART for debug output, and a LED to indicate status.
 	
-	// Start UART.
-	// Nucleo-F042K6 (STM32F042): USART2 (TX: PA2 (AF1), RX: PA15 (AF1)).
-	// USART2 is normally connected to USB (ST-Link) on the Nucleo board.
-	//USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 15, 1, 9600, uartCallback);
-	// USART 2, (TX) PA2:1 [A7], (RX) PA3:1 [A2].
-	//USART::startUart(USART_2, GPIO_PORT_A, 2, 1, GPIO_PORT_A, 3, 1, 9600, uartCallback);
-	// USART 1, (TX) PA9:1 [D1], (RX) PA10:1 [D0].
-	//USART::startUart(USART_1, GPIO_PORT_A, 9, 1, GPIO_PORT_A, 10, 1, 9600, uartCallback);
-	
-	// STM32F4-Discovery (STM32F407).
-	// USART2, (TX) PA2:7, (RX) PA3:7.
-	//USART::startUart(USART_2, GPIO_PORT_A, 2, 7, GPIO_PORT_A, 3, 7, 9600, uartCallback);
-	
+	// 1. Set up UART
 	// Nucleo-F746ZG.
 	// USART3, (TX) PD8:7, (RX) PD9:7.
 	USART::startUart(USART_3, GPIO_PORT_D, 8, 7, GPIO_PORT_D, 9, 7, 9600, uartCallback);
 	
-	//const uint8_t led_pin = 3; // Nucleo-f042k6: Port B, pin 3.
-	//const GPIO_ports led_port = GPIO_PORT_B;
-	//const uint8_t led_pin = 13; // STM32F4-Discovery: Port D, pin 13 (orange)
-	//const GPIO_ports led_port = GPIO_PORT_D;
-	const uint8_t led_pin = 7; // Nucleo-F746ZG: Port B, pin 7 (blue)
-	const GPIO_ports led_port = GPIO_PORT_B;
-	//const uint8_t led_pin = 13;	// Blue Pill: Port C, pin 13.
-	//const GPIO_ports led_port = GPIO_PORT_C;
+	// Set up stdout.
+	IO::setStdOutTarget(USART_3);
+	
+	// Start SysTick.
+	McuCore::initSysTick();
+	
+	printf("Starting SCPI server...\n");
+	
+	// 1. Set up LED.
+	uint8_t 	led_pin;
+	GPIO_ports 	led_port;
+	bool		led_enable = true;
+	if (boardLEDs_count > 0) {
+		led_pin = boardLEDs[0].pin.pin;
+		led_port = boardLEDs[0].pin.port;
+		
+		// Set the pin mode on the LED pin and turn it off.
+		GPIO::set_output(led_port, led_pin, GPIO_PULL_UP);
+		GPIO::write(led_port, led_pin, GPIO_LEVEL_LOW);
+	}
+	else {
+		led_enable = false;
+	}
+	
+	printf("Start Ethernet...\n");
 	
 	// Set up LwIP and Ethernet.
 	// Nucleo-F746ZG Ethernet:
@@ -98,11 +96,20 @@ int main() {
 	eth.MDC 	= { GPIO_PORT_C, 1, 11 };
 	Ethernet::startEthernet(eth);
 	
+	printf("Start LwIP and RTOS...\n");
+	
 	// 2. Start RTOS with LwIP.
 	// 		Our callback is called to start additional tasks.
 	CmsisRTOS_config config;
 	config.cb = startTasks;
 	CmsisRTOS::start(config);
+	
+	// Light up LED.
+	if (led_enable) {
+		GPIO::write(led_port, led_pin, GPIO_LEVEL_HIGH);
+	}
+	
+	printf("Ready...\n");
 	
 	while (1) {
 		//

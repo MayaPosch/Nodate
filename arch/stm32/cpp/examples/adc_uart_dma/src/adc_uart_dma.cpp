4149e751a5bc6c5220862d56aa0c4e8d676542ef
Maya Posch <maya@nyanko.ws>
2022-04-20 21:33:45 +0200

    Add LED & UART-based status output.

--- arch/stm32/cpp/examples/adc_uart_dma/src/adc_uart_dma.cpp
+++ arch/stm32/cpp/examples/adc_uart_dma/src/adc_uart_dma.cpp
@@ -15,7 +15,7 @@ volatile bool sampling_adc = false;
 
 
 void uartCallback(char ch) {
-	sampling_toggle = !sampling_toggle;
+	sampling_toggle = true;
 	//USART::sendUart(usartTarget, ch);
 }
 
@@ -26,11 +26,14 @@ const uint32_t half_buf = 512;
 uint16_t ADC_array[buf_len];
 
 USART_def& ud = boardUSARTs[1];
+uint8_t 	led_pin;
+GPIO_ports 	led_port;
 
 
 void buffer_half_filled_cb() {
 	// Copy first half of the buffer to the USART with DMA.
 	DMA_callbacks cbs;
+	GPIO::write(led_port, led_pin, GPIO_LEVEL_HIGH);
 	USART::configureDMAT(ud.usart, (uint32_t*) (&ADC_array), half_buf, cbs);
 }
 
@@ -38,6 +41,7 @@ void buffer_half_filled_cb() {
 void buffer_end_filled_cb() {
 	// Copy second half of the buffer to the USART with DMA.
 	DMA_callbacks cbs;
+	GPIO::write(led_port, led_pin, GPIO_LEVEL_LOW);
 	USART::configureDMAT(ud.usart, (uint32_t*) (&ADC_array) + half_buf, half_buf, cbs);
 }
 
@@ -59,6 +63,23 @@ int main() {
 	//printf("Starting ADC & USART DMA example...\n");
 	//printf("Send any character to toggle sampling.\n");
 	
+	// 1. Set up LED.
+	bool		led_enable = true;
+	if (boardLEDs_count > 0) {
+		led_pin = boardLEDs[0].pin.pin;
+		led_port = boardLEDs[0].pin.port;
+	}
+	else {
+		led_enable = false;
+	}
+	
+	// Set the pin mode on the LED pin and turn it off.
+	GPIO::set_output(led_port, led_pin, GPIO_PULL_UP);
+	GPIO::write(led_port, led_pin, GPIO_LEVEL_LOW);
+	
+	char ch = '1';
+	USART::sendUart(ud.usart, ch);
+	
 	// 2. Set up ADC & DMA.
 	// Configure for continuous mode, single pin/channel.
 	// STM32F042K6 (Nucleo-F042K6): PA0, PA1 => Ch0, Ch1
@@ -72,13 +93,19 @@ int main() {
 	cbs.half = &buffer_half_filled_cb;
 	cbs.filled = &buffer_end_filled_cb;
 	cbs.error = &buffer_error_cb;
-	ADC::configureDMA(ADC_1, (uint32_t*) &ADC_array, buf_len, cbs);
+	
+	ch = '2';
+	USART::sendUart(ud.usart, ch);
 	
 	
 	// 4. Start sampling.
 	sampling_adc = true;
-	ADC::start(ADC_1);
 	
+	// Start DMA-based sampling.
+	ADC::configureDMA(ADC_1, (uint32_t*) &ADC_array, buf_len, cbs);
+	
+	ch = '3';
+	USART::sendUart(ud.usart, ch);
 	
 	while (1) {
 		// 5. Stop sampling if a character is received on the USART & sampling.

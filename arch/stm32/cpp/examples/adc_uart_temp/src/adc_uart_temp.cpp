01e9de1c8c44f497ac7d770146d9494965684059
Maya Posch <maya@nyanko.ws>
2022-06-04 22:35:09 +0200

    Retarget example for STM32F334.

--- arch/stm32/cpp/examples/adc_uart_temp/src/adc_uart_temp.cpp
+++ arch/stm32/cpp/examples/adc_uart_temp/src/adc_uart_temp.cpp
@@ -8,12 +8,17 @@
 #include <nodate.h>
 #include <printf.h>
 
+#ifdef __stm32f0
 // Reference: RM0091, A.7.16.
 // Note: calibration address may differ for other MCUs.
 #define TEMP30_CAL_ADDR ((uint16_t*) ((uint32_t) 0x1FFFF7B8))
 #define TEMP110_CAL_ADDR ((uint16_t*) ((uint32_t) 0x1FFFF7C2))
 #define VDD_CALIB ((uint16_t) (330))
 #define VDD_APPLI ((uint16_t) (330))
+#elif defined __stm32f3
+#define TS_CAL_30 ((uint16_t*) ((uint32_t) 0x1FFFF7B8))
+#define TS_CAL_110 ((uint16_t*) ((uint32_t) 0x1FFFF7C2))
+#endif
 
 
 void uartCallback(char ch) {
@@ -38,6 +43,7 @@ int main() {
 	// 2. Set up ADC.
 	ADC::configure(ADC_1, ADC_MODE_SINGLE);
 	ADC::channel(ADC_1, ADC_VSENSE, 7);	// Sample Vsense temperature sensor. Long sampling time.
+	ADC::finishChannelConfig(ADC_1);
 	
 	// 3. Start the ADC.
 	if (!ADC::start(ADC_1)) {
@@ -63,18 +69,30 @@ int main() {
 		printf("Raw: %d.\n", raw);
 		
 		// Debug
+#ifdef __stm32f0
 		printf("C30: %d.\n", *TEMP30_CAL_ADDR);
 		printf("C110: %d.\n", *TEMP110_CAL_ADDR);
+#else
+		printf("C30: %d.\n", *TS_CAL_30);
+		printf("C110: %d.\n", *TS_CAL_110);
+#endif
 		
 		// 6. Calculate Celsius value.
 		// Ref.: RM0091, A.7.16. Adapt for other MCUs.
 		int32_t temperature;
+#ifdef __stm32f0
 		//temperature = (((int32_t) raw * VDD_APPLI / VDD_CALIB) - (int32_t) *TEMP30_CAL_ADDR);
 		temperature = (((int16_t) raw) - *TEMP30_CAL_ADDR);
 		temperature = temperature * (int32_t)(110 - 30);
 		temperature = temperature / (int32_t)(*TEMP110_CAL_ADDR - *TEMP30_CAL_ADDR);
 		temperature = temperature + 30;
 		//temperature = (((110 - 30) * ((int32_t) raw - (int32_t) *TEMP30_CAL_ADDR)) / ((int32_t) *TEMP110_CAL_ADDR - (int32_t) *TEMP30_CAL_ADDR)) + 30;
+#else
+		temperature = (((int16_t) raw) - *TS_CAL_30);
+		temperature = temperature * (int32_t)(110 - 30);
+		temperature = temperature / (int32_t)(*TS_CAL_110 - *TS_CAL_30);
+		temperature = temperature + 30;
+#endif
 		
 		// 7. Print out value.
 		printf("Temp: %d °C.\n", temperature);

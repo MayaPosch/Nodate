be2631f6222c8b1b3502781bb2a15d432ed1ac5d
Maya Posch <maya@nyanko.ws>
2022-10-27 19:41:46 +0200

    Add STM32F4-Discovery board configuration.

---
+++ arch/stm32/cpp/boards/stm32f4-discovery/board_definition.cpp
@@ -0,0 +1,96 @@
+/*
+	board_definition.cpp - board definition file for the F407-based STM32F4-Discovery board.
+	
+	MCU: STM32F407VG
+	
+	2021/02/24, Maya Posch
+*/
+
+
+
+
+#include "../board_types.h"
+#include <rcc.h>
+#include <usart.h>
+
+
+RccSysClockConfig maxSysClockCfg;
+BoardLED boardLEDs[1];
+BoardButton boardButtons[1];
+USART_def boardUSARTs[3];
+
+uint8_t boardLEDs_count = 1;
+uint8_t boardButtons_count = 1;
+uint8_t boardUSART_count = 3;
+
+bool init() {
+	// Target frequency: 168 MHz.
+	// Input: 8 MHz HSE, no bypass.
+	maxSysClockCfg.source 		= RCC_SYSCLOCK_SRC_PLL;
+	maxSysClockCfg.base_freq	= 8000000;
+	maxSysClockCfg.HSE_bypass 	= false;
+	maxSysClockCfg.HSI_enabled	= false;
+	maxSysClockCfg.PLL_enabled = true;
+	maxSysClockCfg.PLL_source	= RCC_PLLCLOCK_SRC_HSE;
+	maxSysClockCfg.PLLM		= 8;
+	maxSysClockCfg.PLLN		= 336;
+	maxSysClockCfg.PLLP		= 2;
+	maxSysClockCfg.PLLQ		= 7;
+	maxSysClockCfg.AHB_prescale	= 1;
+	maxSysClockCfg.APB1_prescale	= 4;
+	maxSysClockCfg.APB2_prescale	= 2;
+	maxSysClockCfg.FLASH_latency	= 5;
+
+	// LED
+	BoardLED bl;
+	bl.pin.port = GPIO_PORT_C;
+	bl.pin.pin = 13;
+	bl.pin.pupd = GPIO_PULL_UP;
+	bl.pin.type = GPIO_PUSH_PULL;
+	bl.pin.speed = GPIO_LOW;
+	bl.rgb = { 0, 0xff, 0 };
+	boardLEDs[0] = bl;
+
+	// Button
+	BoardButton bb;
+	bb.pin = { GPIO_PORT_A, 0, GPIO_PULL_UP };
+	boardButtons[0] = bb;
+	
+	// USART
+	USART_def usart;
+	usart.usart = USART_1;
+	usart.configs = 3;
+	usart.tx = new GpioPinDef[3];
+	usart.tx[0] = { .port = GPIO_PORT_A, .pin = 9, .af = 7 };
+	usart.tx[1] = { .port = GPIO_PORT_A, .pin = 15, .af = 7 };
+	usart.tx[1] = { .port = GPIO_PORT_B, .pin = 6, .af = 7 };
+	usart.rx = new GpioPinDef[3];
+	usart.rx[0] = { .port = GPIO_PORT_A, .pin = 10, .af = 7 };
+	usart.rx[1] = { .port = GPIO_PORT_B, .pin = 3, .af = 7 };
+	usart.rx[1] = { .port = GPIO_PORT_B, .pin = 7, .af = 7 };
+	boardUSARTs[0] = usart;
+	
+	usart.usart = USART_2;
+	usart.configs = 2;
+	usart.tx = new GpioPinDef[2];
+	usart.tx[0] = { .port = GPIO_PORT_A, .pin = 2, .af = 7 };
+	usart.tx[1] = { .port = GPIO_PORT_D, .pin = 5, .af = 7 };
+	usart.rx = new GpioPinDef[2];
+	usart.rx[0] = { .port = GPIO_PORT_A, .pin = 3, .af = 7 };
+	usart.rx[1] = { .port = GPIO_PORT_D, .pin = 6, .af = 7 };
+	boardUSARTs[1] = usart;
+	
+	usart.usart = USART_6;
+	usart.configs = 2;
+	usart.tx = new GpioPinDef[2];
+	usart.tx[0] = { .port = GPIO_PORT_A, .pin = 11, .af = 8 };
+	usart.tx[1] = { .port = GPIO_PORT_C, .pin = 6, .af = 8 };
+	usart.rx = new GpioPinDef[2];
+	usart.rx[0] = { .port = GPIO_PORT_A, .pin = 12, .af = 8 };
+	usart.rx[1] = { .port = GPIO_PORT_C, .pin = 7, .af = 8 };
+	boardUSARTs[2] = usart;
+	
+	return true;
+}
+
+bool initialized = init();
---
+++ arch/stm32/cpp/boards/stm32f4-discovery/board_definition.h
@@ -0,0 +1,44 @@
+/*
+	board_definition.h - board definition file for the F407-based STM32F4-Discovery board.
+	
+	MCU: STM32F407VG
+	
+	2021/02/24, Maya Posch
+*/
+
+#ifndef BOARD_DEFINITION
+#define BOARD_DEFINITION
+
+#include <rcc.h>
+#include <usart.h>
+
+#include "../board_types.h"
+
+// --- CLOCKS ---
+
+// >> Max SysClock Profile <<
+// Maximum System Clock speed configuration.
+extern RccSysClockConfig maxSysClockCfg;
+
+
+// --- UARTS ---
+
+// Define the number and features of the U(S)ARTs on the board.
+extern uint8_t boardUSART_count;
+extern USART_def boardUSARTs[3];
+
+// --- LEDS ---
+
+// Define the number and features of the user-addressable LEDs on the board.
+extern uint8_t boardLEDs_count;
+extern BoardLED boardLEDs[1];
+
+
+// --- BUTTONS ---
+
+// Define the user-defined buttons on the board.
+extern uint8_t boardButtons_count;
+extern BoardButton boardButtons[1];
+
+
+#endif
